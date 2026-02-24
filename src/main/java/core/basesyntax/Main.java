6bde6b29e98a3973f0b4176ced21670a1723ec10
alex <ursusandwolf@protonmail.com>
2026-02-24 17:17:07 +0200

    main-class

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
+import core.basesyntax.inout.Reader;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 /**
  * Feel free to remove this class and create your own.
  */
@@ -8,8 +15,9 @@ public class Main {
     // instances of your classes,
     // and call their methods, but do not write any business logic in the `main` method!
 
-    public static void main(String[] arg) {
+    public static void main(String[] arg) throws IOException {
         // 1. Read the data from the input CSV file
+        List<String> lines = Files.readAllLines(Path.of("data.csv"));
         // 2. Convert the incoming data into FruitTransactions list
         // 3. Create and feel the map with all OperationHandler implementations
         // 4. Process the incoming transactions with applicable OperationHandler implementations
